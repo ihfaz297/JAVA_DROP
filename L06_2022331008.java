03fbf3a3645ab92c643f0671ba241956ce67a8ee
ihfaz297 <adib000297@gmail.com>
2026-09-02 09:17:10 +0600

    felix

---
+++ L06_2022331008.java
@@ -0,0 +1,11 @@
+/**
+ * @LabId: 06
+ * @Date: 2026-09-02
+ * @RegNo: 2022331008
+ * @Section: B
+ */
+public class L06_2022331008 {
+    public static void main(String[] args) {
+        
+    }
+}
