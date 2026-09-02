16573b8c4d8f17ecd9ab68f8558458594675b734
ihfaz297 <adib000297@gmail.com>
2026-09-02 09:28:15 +0600

    ok

--- L06_2022331008.java
+++ L06_2022331008.java
@@ -6,6 +6,21 @@
  */
 public class L06_2022331008 {
     public static void main(String[] args) {
-        
+        P p = new P();
+        P.Q h = new P.Q();
+        p.m();
+        P.R f = p.new R();
+    }
+}
+class P{
+    // static nested
+    static class Q {
+        Q(){System.out.println("Q created");}
+    }
+    // non static
+    class R{R(){System.out.println("R created");}}
+    void m(){
+        Q q  = new Q();
+        R r = new R();
     }
 }
