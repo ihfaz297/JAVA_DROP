f5d1f75d23b9339add4a3661b19858ab5b7aa471
ihfaz297 <adib000297@gmail.com>
2026-09-02 09:45:28 +0600

    inheritance start

--- L06_2022331008.java
+++ L06_2022331008.java
@@ -6,33 +6,52 @@
  */
 public class L06_2022331008 {
     public static void main(String[] args) {
-        P.sm2();
-        P p = new P();
-        P.Q h = new P.Q();
-        p.m();
-        P.R f = p.new R();
+        // P.Q.sqm();
+        // P.sm2();
+        // P p = new P();
+        // P.Q h = new P.Q();
+        // p.m();
+        // P.R f = p.new R();
     }
 }
-class P{    int i=10; static int j = 20;
-    // static nested
-    static class Q {
-        Q(){System.out.println("Q created");
-        //static class can access only static vars
-            System.out.println("j="+j); sm2();
-            //static context can't use non-static
-            //P p = new P(); p.m2(); wont work
-        }
+// class P{   private int i=10; static int j = 20;
+//     // static nested
+
+
+
+//     // inner class can access all vars, including private
+//     static class Q {
+//         Q(){System.out.println("Q created");
+//         //static class can access only static vars
+//             System.out.println("j="+j); sm2();
+//             //static context can't use non-static
+//             //P p = new P(); p.m2(); wont work
+//         }
+//         void qm(){}
+//         static void sqm(){}
+//     }
+//     // non static
+//     class R{R(){System.out.println("R created");
+//     //non-static can access any var unless out of scope
+//         System.out.println("j="+j);
+//         System.out.println("i="+i);
+//     }}
+//     void m(){
+//         Q q  = new Q();
+//         R r = new R();
+//     }
+//     void m2(){System.out.println("P.m2()");}
+//     static void sm2(){System.out.println("P.sm2()");}
+// }
+
+class P{
+    P(){System.out.println("P constructor");}
+}
+class Q extends P{
+    Q(){System.out.println("Q constructor");}
+}
+class I{
+    public static void main(String[] args) {
+        P p = new Q();
     }
-    // non static
-    class R{R(){System.out.println("R created");
-    //non-static can access any var unless out of scope
-        System.out.println("j="+j);
-        System.out.println("i="+i);
-    }}
-    void m(){
-        Q q  = new Q();
-        R r = new R();
-    }
-    void m2(){System.out.println("P.m2()");}
-    static void sm2(){System.out.println("P.sm2()");}
 }
